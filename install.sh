2bd9921ec1e14932708462c1a7fe31f92fb04195
Jothi Prasath <jothiprasath2@gmail.com>
2023-08-04 18:41:05 +0530

    optimize code

--- install.sh
+++ install.sh
@@ -1,13 +1,9 @@
 pkg update
-pkg upgrade
-pkg install python
-pkg install python-numpy
-pkg update
-pkg install ffmpeg
-pip install -U yt-dlp
-pip install shazamio
-pkg install termux-api
-mkdir ~/bin
-touch ~/bin/termux-url-opener
-echo "python3 $(pwd)/share_recognize.py \$1" >> ~/bin/termux-url-opener
-
+pkg upgrade -y
+pkg install -y python \
+               python-numpy \
+               ffmpeg \
+               termux-api
+pip install -U yt-dlp shazamio
+mkdir -p ~/bin
+echo "python3 $(pwd)/share_recognize.py \$1" > ~/bin/termux-url-opener
